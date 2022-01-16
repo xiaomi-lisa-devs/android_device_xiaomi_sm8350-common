bee2d7ad27817fcaeda0e7f7586acd87a84399a4
ItsVixano <giovanniricca@protonmail.com>
2022-01-16 17:39:51 +0100

    sm8350-common: Update blobs from missi-user 11 RKQ1.201112.002 V12.5.8.0.RKKEUXM release-keys
    * Import missing drm libs
    * Also reorder libs
    
    Change-Id: I76f181ccfd9c6c5c3429d3454954dff193087d80

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -186,7 +186,7 @@ TARGET_USES_MKE2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-07-01
+VENDOR_SECURITY_PATCH := 2021-12-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
