744d0919701013cab1f58dfa546729706c28f67f
ItsVixano <giovanniricca@protonmail.com>
2022-02-07 19:21:04 +0100

    sm8350-common: Decommonize ramdisk kernel modules
    Change-Id: I5cefe9db56cdd4e9adef0f0fecb60c189d76c2b0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,14 +99,6 @@ BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
-# Kernel modules
-BOOT_KERNEL_MODULES := \
-    hwid.ko \
-    focaltech_touch.ko \
-    goodix_core.ko \
-    xiaomi_touch.ko
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
-
 # HIDL
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
