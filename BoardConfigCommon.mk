36f07e686330ed7f879e12b8aabc7e1c56808b57
Giovanni Ricca <giovanniricca@proton.me>
2022-09-17 20:21:03 +0200

    sm8350-common: Update blobs to odin V13.1.22.9.12.DEV
    Change-Id: I4e367fb41e8b153719604ce48ef98f7ad339ecd3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,7 +180,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-07-01
+VENDOR_SECURITY_PATCH := 2022-09-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
